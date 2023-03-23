f7babc6a77e37807a2da4717244141be3e149dda
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-03-23 17:27:31 +0100

    [FIX] frame and database creation

--- homepage.py
+++ homepage.py
@@ -1,49 +1,50 @@
-from Settings import *
 from inscription import Inscription
-from Connexion import Connexion
+from connexion import Connexion
 from database import Database
+from settings import *
 
-class Homepage():
+
+class Homepage:
     def __init__(self):
         self.win = tk.Tk()
         self.win.geometry('800x600')
         self.win.title('myDiscord')
-        
-        # self.database = Database()
+
+        self.database = Database()
 
         # Theme import : 
         self.win.tk.call('source', 'Tkinter_theme/forest-dark.tcl')
         ttk.Style().theme_use('forest-dark')
 
+    def draw_homepage(self):
+        self.frame = ttk.Frame(self.win)
+        self.frame.pack(pady=200)
+
+        title = ttk.Label(self.frame, text ="MyDiscord", font=MEDIUM)
+        title.pack()
+
+        button_frame = ttk.Frame(self.frame)
+        button_frame.pack(pady=50)
+
+        inscription = ttk.Button(button_frame, text="Inscription", command=lambda:[self.clear_frame(), Inscription(self.win)])
+        inscription.pack(side='left', padx=20)
+
+        connexion = ttk.Button(button_frame, text="Connexion", command=lambda:[self.clear_frame(), Connexion(self.win)])
+        connexion.pack(side='right', padx=20)
+
+    def clear_frame(self):
+        for element in self.frame.winfo_children():
+            element.destroy()
+
+    def create_database(self):
+        self.database.create_my_discord()
+
+    def run(self):
         self.draw_homepage()
         self.win.mainloop()
-
-    def draw_homepage(self):
-        background = tk.PhotoImage( file='pictures/Background.png', master=self.win)
-        background_label = ttk.Label(self.win, image=background)
-        background_label.place(x=0, y=0)
-        # background_canvas = tk.Canvas(self.win, width=800, height=600)
-        # background_canvas.pack(fill='both', expand=True)
-        # background_canvas.create_image(0, 0, image=background, anchor='nw')
-
-        title_frame = ttk.Frame(self.win)
-        title_frame.pack(pady=200)
-
-        title = ttk.Label(title_frame, text = "MyDiscord", font=MEDIUM)
-        title.pack(pady=20)
-
-        button_frame = ttk.Frame(title_frame)
-        button_frame.pack(padx=100, pady=30)
-        inscription = ttk.Button(button_frame, text="Inscription", command=lambda : Inscription(self.win))
-        inscription.pack(side='left')
-        connexion = ttk.Button(button_frame, text="Connexion", command=lambda : Connexion(self.win))
-        connexion.pack(side='right', padx=40)
-
-    # def create_database(self):
-    #     self.database.create_my_discord()
 
 
 if __name__ == '__main__':
     homepage = Homepage()
-    # homepage.create_database()
-    
+    homepage.create_database()
+    homepage.run()

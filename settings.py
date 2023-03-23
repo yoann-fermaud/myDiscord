08559cee511608efe0ab259640dbfab61e511a0b
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-03-23 17:27:54 +0100

    [ADD] import

--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import scrolledtext
+from tkinter import simpledialog
 import re
 import hashlib
+import mysql.connector
 
 LITTLE = ('Terminal', 14)
 MEDIUM = ('Terminal', 40)

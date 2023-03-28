49bfcea25cc7e0205f7153390ff26d621d1fe9ec
aurore-thomas <aurore.thomas@laplateforme.io>
2023-03-28 10:10:36 +0200

    [ADD] New import

--- settings.py
+++ settings.py
@@ -2,9 +2,12 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
 from tkinter import simpledialog
+import socket
+import threading
 import re
-import hashlib
+import bcrypt
 import mysql.connector
 
+VERY_LITTLE = ('Terminal', 8)
 LITTLE = ('Terminal', 14)
 MEDIUM = ('Terminal', 40)

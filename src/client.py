8b0cb095a97d40420de6af4717460954291ee9b0
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-03-30 11:43:09 +0200

    [ADD] client

---
+++ src/client.py
@@ -0,0 +1,40 @@
+from src.settings import *
+
+
+class Clients:
+    def __init__(self, host, port, user):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+
+        self.nickname = user
+
+    def write(self):
+        message = f"{self.nickname} : {self.input_area.get('1.0', 'end')}"
+        self.sock.send(message.encode('utf-8'))
+        self.input_area.delete('1.0', 'end')
+
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        self.sock.close()
+        exit(0)
+
+    def receive(self):
+        while self.running:
+            try:
+                message = self.sock.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.sock.send(self.nickname.encode('utf-8'))
+                else:
+                    if self.gui_done:
+                        self.text_area.config(state='normal')
+                        self.text_area.insert("end", message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state='disabled')
+
+            except ConnectionAbortedError:
+                break
+            except:
+                print("Error")
+                self.sock.close()
+                break
